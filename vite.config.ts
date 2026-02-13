142e43ff272f1d3113ec2e64ed5ffbe2a1664187
signalwerk <sh@signalwerk.ch>
2026-02-13 02:57:11 +0100

    ADD: github action for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const isGithubPagesBuild =
+  process.env.GITHUB_ACTIONS === "true" && Boolean(repoName);
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: isGithubPagesBuild ? `/${repoName}/` : "/",
   plugins: [react()],
 });
